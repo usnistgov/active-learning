eb0a6fdd092abf1119f79c05804b9ec7d35bb647
Daniel Wheeler <daniel.wheeler2@gmail.com>
2023-09-13 09:42:24 -0400

    add results from mr-french

--- 3D/submit.sh
+++ 3D/submit.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --partition=rack4               # -p, first available from the list of partitions
-#SBATCH --time=04:00:00                 # -t, time (hh:mm:ss or dd-hh:mm:ss)
+#SBATCH --time=08:00:00                 # -t, time (hh:mm:ss or dd-hh:mm:ss)
 #SBATCH --nodes=1                       # -N, total number of machines
 #SBATCH --ntasks=1                      # -n, 64 MPI ranks per Opteron machine
 #SBATCH --cpus-per-task=10               # threads per MPI rank
-#SBATCH --job-name=test_active_learning                 # -J, for your records
+#SBATCH --job-name=nu-1_0_r2_cutoff-20                 # -J, for your records
 #SBATCH --chdir=/working/wd15/active-learning/3D   # -D, full path to an existing directory
 #SBATCH --qos=test
 #SBATCH --mem=0G
 #SBATCH --output=log/slurm-%j.out
 
-job_name="job_2023-09-12_id-${SLURM_JOB_ID}_nu-0_5_r2_cutoff-20_v001"
-reason="test new submit.sh file"
-nu=0.5
+job_name="job_2023-09-12_id-${SLURM_JOB_ID}_nu-1_0_r2_cutoff-20_v001"
+reason="run with nu=1.0 and cutoff of 20"
+nu=1.0
 cutoff=20
 
 ~/bin/nix-root nix develop ../flake.nix --command bash -c "snakemake \
