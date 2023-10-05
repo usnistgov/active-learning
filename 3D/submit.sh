2dbbaaa3c82854a33bfa256a9251f5589a424782
Daniel Wheeler <daniel.wheeler2@gmail.com>
2023-10-05 13:02:18 -0400

    Add results from simulation

--- 3D/submit.sh
+++ 3D/submit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --partition=rack4               # -p, first available from the list of partitions
+#SBATCH --partition=rack2e               # -p, first available from the list of partitions
 #SBATCH --time=08:00:00                 # -t, time (hh:mm:ss or dd-hh:mm:ss)
 #SBATCH --nodes=1                       # -N, total number of machines
 #SBATCH --ntasks=1                      # -n, 64 MPI ranks per Opteron machine
 #SBATCH --cpus-per-task=10               # threads per MPI rank
-#SBATCH --job-name=wasserstein                 # -J, for your records
+#SBATCH --job-name=job_2023-10-04_full-no-represent_v000 # -J, for your records
 #SBATCH --chdir=/working/wd15/active-learning/3D   # -D, full path to an existing directory
 #SBATCH --qos=test
 #SBATCH --mem=0G
@@ -17,8 +17,8 @@ export MKL_NUM_THREADS=$omp_threads
 export VECLIB_MAXIMUM_THREADS=$omp_threads
 export NUMEXPR_NUM_THREADS=$omp_threads
 
-job_name="job_2023-09-15_wasserstein_v000"
-reason="Generate 3D Wasserstein distance plot"
+job_name="job_2023-10-04_full-no-represent_v000"
+reason="Full run without Wasserstein calculation which is too expensive"
 nu=1.5
 cutoff=20
 scoring="mae"
