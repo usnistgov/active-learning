394e5799bb693e7ea56355dbd5859875e4d53cf4
Daniel Wheeler <daniel.wheeler2@gmail.com>
2024-01-09 14:51:06 -0500

    calculate 3D cutoffs

--- 3D/submit-1600.sh
+++ 3D/submit-1600.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=05-00:00:00                 # -t, time (hh:mm:ss or dd-hh:mm:ss)
 #SBATCH --nodes=1                       # -N, total number of machines
 #SBATCH --ntasks=1                      # -n, 64 MPI ranks per Opteron machine
-#SBATCH --cpus-per-task=10               # threads per MPI rank
+#SBATCH --cpus-per-task=1               # threads per MPI rank
 #SBATCH --job-name=job_2023-10-12_query-1600_v000 # -J, for your records
 #SBATCH --chdir=/working/wd15/active-learning/3D   # -D, full path to an existing directory
 #SBATCH --qos=test
